43c29753ac5c2c365c38c3e85cc52376d7588c9c
Ish Purwar <ishpurwar121@gmail.com>
2024-07-09 23:17:33 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import RevenueCard from './Components/RevenueCard'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
+  )
+}
+
+export default App
